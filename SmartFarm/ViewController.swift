f0965f5c55ed6471e0ac9c429a27e60ccdaaa7eb
Yinhuan Yuan <yhyuan@gmail.com>
2015-08-25 16:22:25 -0400

    Initial Commit

---
+++ SmartFarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartFarm
+//
+//  Created by Yuan Yinhuan on 15/8/25.
+//  Copyright (c) 2015年 Jun Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
